18e546c9c74e1354f12e4ebf43e5e03e678ff888
Adriano <adricatena@gmail.com>
2023-09-05 21:37:01 -0300

    redirect on logout

--- src/components/logout-button.tsx
+++ src/components/logout-button.tsx
@@ -1,15 +1,23 @@
 import { AuthController } from "@/entities/auth/auth.controller";
 import { Button } from "@mantine/core";
 import { IconDoorExit } from "@tabler/icons-react";
+import { useRouter } from "next/router";
 
 function LogoutButton() {
+  const router = useRouter();
+
+  async function handleClick() {
+    await AuthController.logout();
+    router.push("/login");
+  }
+
   return (
     <Button
       size="xs"
       color="dark"
       leftIcon={<IconDoorExit size={15} />}
       className="m-2"
-      onClick={async () => await AuthController.logout()}
+      onClick={handleClick}
     >
       Cerrar sesion
     </Button>
