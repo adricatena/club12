f121be0245355d0dc6e9f9f4888336fa47dee2f9
Adriano <adricatena@gmail.com>
2023-10-20 14:51:22 -0300

    rename pages

--- src/resources/team/service.ts
+++ src/resources/team/service.ts
@@ -53,6 +53,127 @@ const TeamService = {
 
     return { ok: true, message: "Equipo creado correctamente" };
   },
+  async updateTeam(client: SupabaseClient<Database>) {},
 };
+/* async updatePlayer(
+    client: SupabaseClient<Database>,
+    {
+      data,
+      oldData,
+      sportsFromDb,
+    }: {
+      data: UpdatePlayer;
+      oldData: {
+        playerSports: PlayerSportFromDb[] | null;
+      };
+      sportsFromDb: SportFromDb[];
+    },
+  ): Promise<Return> {
+    // actualizar tabla players
+    const { data: updatedPlayerData, error } = await client
+      .from("players")
+      .update({
+        active: data.active,
+        birthdate: data.birthdate,
+        cellphone: data.cellphone,
+        dni: Number(data.dni),
+        email: data.email,
+        lastname: data.lastname,
+        name: data.name,
+        observations: data.observations,
+      })
+      .eq("dni", data.dni)
+      .select("id");
+    if (error) return { ok: false, message: error.message };
+
+    // actualizar tabla player_sport
+    const { activeSports, federatedSports, dni, name, lastname } = data;
+    const { playerSports: oldPlayerSports } = oldData;
+    const player_id = updatedPlayerData[0].id;
+    // si no tenia deportes activos previamente directamente agregamos los nuevos
+    if (!oldPlayerSports || !oldPlayerSports.length) {
+      const { error } = await insertPlayerSports({
+        client,
+        activeSports,
+        federatedSports,
+        sportsFromDb,
+        player_id,
+      });
+      if (error) return { ok: false, message: error.message };
+    } else {
+      // obtenemos nuevos deportes activos (si es que hay)
+      const newActivesSportsNames = activeSports.filter((playerSportName) => {
+        const alreadyActiveSport = oldPlayerSports.some(
+          (oldPlayerSport) => oldPlayerSport.name === playerSportName,
+        );
+        return !alreadyActiveSport;
+      });
+      // guardamos nuevos deportes activos (si es que hay)
+      if (newActivesSportsNames.length) {
+        const { error } = await insertPlayerSports({
+          client,
+          activeSports: newActivesSportsNames,
+          federatedSports,
+          sportsFromDb,
+          player_id,
+        });
+        if (error) return { ok: false, message: error.message };
+      }
+
+      // obtenemos y eliminamos deportes activos a eliminar (si es que hay)
+      for (const oldPlayerSport of oldPlayerSports) {
+        const alreadyActiveSport = activeSports.some(
+          (activeSport) => activeSport === oldPlayerSport.name,
+        );
+        if (!alreadyActiveSport) {
+          const { id: playerSportIdToDelete } = oldPlayerSport;
+          // obtenemos de la base si el jugador pertenece a un equipo del deporte a eliminar
+          const { data: playerTeamToDelete } = await client
+            .from("players_teams")
+            .select("team_id, teams!inner(sport_id)")
+            .eq("player_id", player_id)
+            .eq("teams.sport_id", playerSportIdToDelete);
+          if (playerTeamToDelete) {
+            // si tiene algun equipo de ese deporte lo borramos
+            await client
+              .from("players_teams")
+              .delete()
+              .eq("player_id", player_id)
+              .eq("team_id", playerTeamToDelete[0].team_id);
+          }
+          // borramos el deporte del jugador
+          await client
+            .from("players_sports")
+            .delete()
+            .eq("player_id", player_id)
+            .eq("sport_id", playerSportIdToDelete);
+        }
+        // comparamos los nuevos deportes federados con los que ya estaban guardados
+        const nowIsFederated = federatedSports.includes(oldPlayerSport.name);
+        if (nowIsFederated !== oldPlayerSport.federated) {
+          await client
+            .from("players_sports")
+            .update({
+              federated: nowIsFederated,
+            })
+            .eq("player_id", player_id)
+            .eq("sport_id", oldPlayerSport.id);
+        }
+      }
+    }
+    // actualizar foto si es que cambio
+    const { photo, photoSrc } = data;
+    if (photo) {
+      const filename = getPlayerFilename(dni, name, lastname);
+      await uploadPhoto({ client, filename, photo, bucket: "players" });
+    } else if (!photoSrc) {
+      const fileName = getPlayerFilename(data.dni, data.name, data.lastname);
+      await client.storage.from("players").remove([fileName]);
+    }
+    return {
+      ok: true,
+      message: `El jugador ${data.name} ${data.lastname} con DNI ${data.dni} se modifico correctamente!`,
+    };
+  }, */
 
 export default TeamService;
