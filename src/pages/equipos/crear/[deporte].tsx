524c04f3544997f011aab0f7152775b17437e3dc
martin <martinnfontana@gmail.com>
2023-10-24 19:00:20 -0300

    fix styles createTeams

--- src/pages/equipos/crear/[deporte].tsx
+++ src/pages/equipos/crear/[deporte].tsx
@@ -8,11 +8,19 @@ import SportService from "@/resources/sport/service";
 import { SportFromDb } from "@/resources/sport/types";
 import TeamService from "@/resources/team/service";
 import { NewTeam } from "@/resources/team/types";
-import { Button, Checkbox, Input, Paper, Table, TextInput } from "@mantine/core";
+import {
+  Alert,
+  Button,
+  Checkbox,
+  Input,
+  Paper,
+  Table,
+  TextInput,
+} from "@mantine/core";
 import { useForm } from "@mantine/form";
 import type { GetServerSideProps } from "next";
+import Link from "next/link";
 import { useState } from "react";
-
 
 interface Props {
   sportFromDb: SportFromDb | null;
@@ -26,20 +34,24 @@ export const getServerSideProps: GetServerSideProps<Props> = async (
     props: {
       playersFromDb: [],
       sportFromDb: null,
-    }
-  }
+    },
+  };
   const sportFromUrl = context.query.deporte;
-  if (!sportFromUrl || typeof (sportFromUrl) !== 'string') return emptyReturn;
+  if (!sportFromUrl || typeof sportFromUrl !== "string") return emptyReturn;
 
   const client = getServerClient(context);
   const { data: sportsFromDb } = await SportService.getSports(client);
   if (!sportsFromDb) return emptyReturn;
 
-  const sportFromDb = sportsFromDb.filter((sport) => sport.name.toLowerCase() === sportFromUrl)[0]
+  const sportFromDb = sportsFromDb.filter(
+    (sport) => sport.name.toLowerCase() === sportFromUrl,
+  )[0];
   if (!sportFromDb) return emptyReturn;
 
-  const { data: playersFromDb } = await PlayerService.getPlayersSport(client, { sport_id: sportFromDb.id,free_agents:true })
-
+  const { data: playersFromDb } = await PlayerService.getPlayersSport(client, {
+    sport_id: sportFromDb.id,
+    free_agents: true,
+  });
 
   return {
     props: {
@@ -50,22 +62,23 @@ export const getServerSideProps: GetServerSideProps<Props> = async (
 };
 
 function CreateTeam({ playersFromDb, sportFromDb }: Props) {
-  const { setValues, reset, onSubmit, getInputProps, values } = useForm<NewTeam>({
-    initialValues: {
-      name: "",
-      sport: sportFromDb || { name: "", id: "" },
-      photoSrc: "",
-      players: [],
-    },
-    validate: {
-      name: (name) => {
-        if (!name || name.trim() === "") {
-          return "El nombre del equipo es obligatorio";
-        }
-        return null;
+  const { setValues, reset, onSubmit, getInputProps, values } =
+    useForm<NewTeam>({
+      initialValues: {
+        name: "",
+        sport: sportFromDb || { name: "", id: "" },
+        photoSrc: "",
+        players: [],
       },
-    },
-  });
+      validate: {
+        name: (name) => {
+          if (!name || name.trim() === "") {
+            return "El nombre del equipo es obligatorio";
+          }
+          return null;
+        },
+      },
+    });
 
   const [isLoadingForm, setIsLoadingForm] = useState(false);
 
@@ -85,49 +98,50 @@ function CreateTeam({ playersFromDb, sportFromDb }: Props) {
     });
   }
 
-   async function handleSubmit(values: NewTeam) {
-    console.log('Datos a enviar:', values);
-     setIsLoadingForm(true);
-       const { ok, message } = await TeamService.createTeam( 
-        browserClient,
-        values,
-      );
+  async function handleSubmit(values: NewTeam) {
+    console.log("Datos a enviar:", values);
+    setIsLoadingForm(true);
+    const { ok, message } = await TeamService.createTeam(browserClient, values);
 
-if(ok){
-       toast.success(
-         "Equipo creado correctamente",
-         `El equipo ${values.name} se creo correctamente`,
-       );
-       reset();}
-       else toast.error("Error creando el equipo", message);
-     
-       setIsLoadingForm(false);
-   }
+    if (ok) {
+      toast.success(
+        "Equipo creado correctamente",
+        `El equipo ${values.name} se creo correctamente`,
+      );
+      reset();
+    } else toast.error("Error creando el equipo", message);
+
+    setIsLoadingForm(false);
+  }
 
   const [players, setPlayers] = useState(
-    playersFromDb?.filter((player) =>
-      player.active
-    )
+    playersFromDb?.filter((player) => player.active),
   );
   const [team, setTeam] = useState([] as PlayerFromDb[]);
-  const [selectedPlayersInTeam, setSelectedPlayersInTeam] = useState<string[]>([]);
+  const [selectedPlayersInTeam, setSelectedPlayersInTeam] = useState<string[]>(
+    [],
+  );
   const [selectedPlayers, setSelectedPlayers] = useState<string[]>([]);
   const [isPlayersTableSelected, setIsPlayersTableSelected] = useState(false);
 
   const handleAddToTeam = () => {
-    const playersToAdd = players?.filter((player) =>
-      selectedPlayers.includes(player.id)
-    ) || [];
+    const playersToAdd =
+      players?.filter((player) => selectedPlayers.includes(player.id)) || [];
     setTeam([...team, ...playersToAdd]);
-    values.players = [...team, ...playersToAdd]
-    setPlayers(players?.filter((player) => !selectedPlayers.includes(player.id)));
-    setSelectedPlayersInTeam([...selectedPlayersInTeam, ...playersToAdd.map((player) => player.id)]);
+    values.players = [...team, ...playersToAdd];
+    setPlayers(
+      players?.filter((player) => !selectedPlayers.includes(player.id)),
+    );
+    setSelectedPlayersInTeam([
+      ...selectedPlayersInTeam,
+      ...playersToAdd.map((player) => player.id),
+    ]);
     setSelectedPlayers([]);
   };
 
   const handleRemoveFromTeam = () => {
     const playersToRemove = team.filter((player) =>
-      selectedPlayers.includes(player.id)
+      selectedPlayers.includes(player.id),
     );
 
     setTeam(team.filter((player) => !selectedPlayers.includes(player.id)));
@@ -140,7 +154,9 @@ if(ok){
       // Estás en la tabla de jugadores
       if (selectedPlayersInTeam.includes(playerId)) {
         // Si el jugador ya está en el equipo, lo deseleccionamos
-        setSelectedPlayersInTeam(selectedPlayersInTeam.filter((id) => id !== playerId));
+        setSelectedPlayersInTeam(
+          selectedPlayersInTeam.filter((id) => id !== playerId),
+        );
       } else {
         // Si el jugador no está en el equipo, lo seleccionamos
         setSelectedPlayersInTeam([...selectedPlayersInTeam, playerId]);
@@ -160,18 +176,34 @@ if(ok){
   const [searchPlayers, setSearchPlayers] = useState<string>("");
   const [searchTeam, setSearchTeam] = useState<string>("");
 
-  const filteredPlayers = players?.filter((player) =>
-    player.name.toLowerCase().includes(searchPlayers.toLowerCase()) ||
-    player.lastname.toLowerCase().includes(searchPlayers.toLowerCase()) ||
-    player.dni.toString().toLowerCase().includes(searchPlayers.toLowerCase())
+  const filteredPlayers = players?.filter(
+    (player) =>
+      player.name.toLowerCase().includes(searchPlayers.toLowerCase()) ||
+      player.lastname.toLowerCase().includes(searchPlayers.toLowerCase()) ||
+      player.dni.toString().toLowerCase().includes(searchPlayers.toLowerCase()),
   );
 
-  const filteredTeam = team.filter((player) =>
-    player.name.toLowerCase().includes(searchTeam.toLowerCase()) ||
-    player.lastname.toLowerCase().includes(searchTeam.toLowerCase()) ||
-    player.dni.toString().toLowerCase().includes(searchTeam.toLowerCase())
+  const filteredTeam = team.filter(
+    (player) =>
+      player.name.toLowerCase().includes(searchTeam.toLowerCase()) ||
+      player.lastname.toLowerCase().includes(searchTeam.toLowerCase()) ||
+      player.dni.toString().toLowerCase().includes(searchTeam.toLowerCase()),
   );
 
+  if (!playersFromDb || !sportFromDb)
+    return (
+      <Layout>
+        <section className="mx-auto text-center">
+          <Alert className="mx-auto mt-4 w-1/2" color="red" title="Error">
+            Error al cargar los datos del equipo. Por favor regresa al menu
+            anterior
+          </Alert>
+          <Button component={Link} href="/equipos/crear" className="mt-4">
+            Volver
+          </Button>
+        </section>
+      </Layout>
+    );
   return (
     <Layout
       breadcrumbs={[
@@ -181,23 +213,39 @@ if(ok){
     >
       <form
         className="flex w-full items-stretch gap-7 self-center p-4"
-         onSubmit={onSubmit(handleSubmit)}
+        onSubmit={onSubmit(handleSubmit)}
       >
         <section className="flex w-full flex-col gap-5">
-          <TextInput
-            label="Nombre del Equipo"
-            placeholder="Boston Celtics"
-            required
-            {...getInputProps("name")}
-          />
-          <div className="flex justify-center items-center space-x-4">
-            <Paper shadow="xs" p="xs" className="w-full" style={{ minHeight: "300px", minWidth: "300px" }}>
-              <h2 className="text-lg font-semibold mb-4">Jugadores</h2>
+          <div className=" flex items-center justify-between gap-10">
+            <TextInput
+              label="Nombre del Equipo"
+              placeholder="Boston Celtics"
+              className="w-full"
+              required
+              {...getInputProps("name")}
+            />
+            <p>Deporte {sportFromDb?.name}</p>
+            <InputPhoto
+              photoSrc={values.photoSrc}
+              onClickFileButton={handleChangePhoto}
+              onClickDeleteButton={handleClickDeletePhoto}
+            />
+          </div>
+          <div className="flex items-center justify-center space-x-4">
+            <Paper
+              shadow="xs"
+              p="xs"
+              className="w-full"
+              style={{ minHeight: "300px", minWidth: "300px" }}
+            >
+              <h2 className="mb-4 text-lg font-semibold">Jugadores</h2>
               <Input
                 placeholder="Buscar jugador"
                 rightSection={<i className="fas fa-search"></i>}
                 value={searchPlayers}
-                onChange={(event) => setSearchPlayers(event.currentTarget.value)}
+                onChange={(event) =>
+                  setSearchPlayers(event.currentTarget.value)
+                }
               />
               <div style={{ maxHeight: "350px", overflowY: "auto" }}>
                 <Table className="w-full">
@@ -241,8 +289,13 @@ if(ok){
                 Quitar
               </Button>
             </div>
-            <Paper shadow="xs" p="xs" className="w-full" style={{ minHeight: "300px", minWidth: "300px" }}>
-              <h2 className="text-lg font-semibold mb-4">Equipo</h2>
+            <Paper
+              shadow="xs"
+              p="xs"
+              className="w-full"
+              style={{ minHeight: "300px", minWidth: "300px" }}
+            >
+              <h2 className="mb-4 text-lg font-semibold">Equipo</h2>
               <Input
                 placeholder="Buscar jugador"
                 rightSection={<i className="fas fa-search"></i>}
@@ -273,22 +326,8 @@ if(ok){
                     ))}
                   </Table.Tbody>
                 </Table>
-
               </div>
             </Paper>
-          </div>
-        </section>
-        <section className="grid">
-          <div className="flex flex-col gap-5">
-            <InputPhoto
-              photoSrc={values.photoSrc}
-              onClickFileButton={handleChangePhoto}
-              onClickDeleteButton={handleClickDeletePhoto}
-            />
-            <div className="flex flex-col gap-2">
-              <label className="text-gray-600">Deporte:</label>
-              <a className="text-xl font-semibold">{sportFromDb?.name}</a>
-            </div>
           </div>
           <Button
             type="submit"
