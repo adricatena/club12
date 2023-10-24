49cf430012117cea7e1564f25381f7706ba18024
Adriano <adricatena@gmail.com>
2023-10-24 19:07:35 -0300

    remove unused variables

--- src/pages/jugadores/index.tsx
+++ src/pages/jugadores/index.tsx
@@ -4,9 +4,9 @@ import { getServerClient } from "@/database/clients";
 import PlayerService from "@/resources/player/service";
 import { PlayerFromDb } from "@/resources/player/types";
 import { ActionIcon, NumberInput } from "@mantine/core";
-import { IconArrowDown, IconArrowUp, IconSearch } from "@tabler/icons-react";
+import { IconSearch } from "@tabler/icons-react";
 import type { GetServerSideProps } from "next";
-import { FormEvent, MouseEvent, useRef, useState } from "react";
+import { FormEvent, useRef, useState } from "react";
 
 interface Props {
   playersFromDb: PlayerFromDb[] | null;
@@ -24,61 +24,10 @@ export const getServerSideProps: GetServerSideProps<Props> = async (
   };
 };
 
-type ColumnKey = "dni" | "name" | "lastname" | "birthdate";
-
-const columnsKeys: {
-  key: ColumnKey;
-  label: string;
-}[] = [
-  { key: "dni", label: "DNI" },
-  { key: "name", label: "Nombre" },
-  { key: "lastname", label: "Apellido" },
-  { key: "birthdate", label: "Nacimiento" },
-];
-
-const enum OrderMethods {
-  default,
-  ascendent,
-  descendent,
-}
-
-const ORDER_METHODS = {
-  [OrderMethods.default]: OrderMethods.descendent,
-  [OrderMethods.ascendent]: OrderMethods.descendent,
-  [OrderMethods.descendent]: OrderMethods.ascendent,
-};
-
 export default function Players({ playersFromDb }: Props) {
-  const [orderBy, setOrderBy] = useState<ColumnKey>();
-  const [orderMethod, setOrderMethod] = useState(OrderMethods.default);
   const [players, setPlayers] = useState<PlayerFromDb[]>(playersFromDb ?? []);
 
   const searchPlayerRef = useRef<HTMLInputElement>(null);
-
-  const Arrow =
-    orderMethod === OrderMethods.ascendent ? IconArrowDown : IconArrowUp;
-
-  function handleClickColumnHeader(event: MouseEvent<HTMLTableCellElement>) {
-    const { id } = event.currentTarget;
-    let newOrderMethod: OrderMethods;
-    if (id === orderBy) {
-      newOrderMethod = ORDER_METHODS[orderMethod];
-    } else {
-      newOrderMethod = ORDER_METHODS[OrderMethods.default];
-      setOrderBy(id as ColumnKey);
-    }
-    const sortedPlayers = [...players];
-    sortedPlayers.sort((prevPlayer, nextPlayer) => {
-      const prevPlayerValue = prevPlayer[id as ColumnKey];
-      const nextPlayerValue = nextPlayer[id as ColumnKey];
-
-      const order = prevPlayerValue > nextPlayerValue;
-      if (newOrderMethod === OrderMethods.ascendent) return order ? 1 : -1;
-      return order ? -1 : 1;
-    });
-    setPlayers(sortedPlayers);
-    setOrderMethod(newOrderMethod);
-  }
 
   function handleSearchPlayerSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
