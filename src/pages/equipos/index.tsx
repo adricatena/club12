6f7ab9bdd8eb5fc7d11d6af68968cbe535c52b33
martin <martinnfontana@gmail.com>
2023-12-27 11:55:04 -0300

    add pagination in teams table

--- src/pages/equipos/index.tsx
+++ src/pages/equipos/index.tsx
@@ -6,15 +6,17 @@ import SportService from "@/resources/sport/service";
 import type { SportFromDb } from "@/resources/sport/types";
 import TeamService from "@/resources/team/service";
 import type { TeamFromDb } from "@/resources/team/types";
-import { ActionIcon, Select, TextInput } from "@mantine/core";
+import { ActionIcon, Pagination, Select, TextInput } from "@mantine/core";
 import { IconSearch } from "@tabler/icons-react";
 import { type GetServerSideProps } from "next";
-import { useState } from "react";
+import router from "next/router";
+import { useEffect, useState } from "react";
 
 interface Props {
   defaultSportFromDb: SportFromDb | null;
   teamsFromDb: TeamFromDb[] | null;
   sportsFromDb: SportFromDb[];
+  totalTeams: number;
 }
 export const getServerSideProps: GetServerSideProps<Props> = async (
   context,
@@ -25,6 +27,7 @@ export const getServerSideProps: GetServerSideProps<Props> = async (
       teamsFromDb: null,
       defaultSportFromDb: null,
       sportsFromDb: [],
+      totalTeams: 0,
     },
   };
   const client = getServerClient(context);
@@ -46,29 +49,64 @@ export const getServerSideProps: GetServerSideProps<Props> = async (
       teamsFromDb,
       defaultSportFromDb,
       sportsFromDb,
+      totalTeams: teamsFromDb.length,
     },
   };
 };
 
-function Teams({ teamsFromDb, defaultSportFromDb, sportsFromDb }: Props) {
-  const [teams, setTeams] = useState(teamsFromDb);
+function Teams({
+  teamsFromDb,
+  defaultSportFromDb,
+  sportsFromDb,
+  totalTeams,
+}: Props) {
+  const [teams, setTeams] = useState<TeamFromDb[]>([]);
   const [selectedSport, setSelectedSport] = useState(defaultSportFromDb);
   const [searchTeam, setSearchTeam] = useState("");
+  const [totalTeam, setTotalTeam] = useState(totalTeams);
+  const [page, setPage] = useState(1);
+  const [amount, setAmount] = useState(1);
 
-  async function handleChangeSportSelect(selectedSportId: string) {
-    setSelectedSport(
-      sportsFromDb.find((sportFromDb) => sportFromDb.id === selectedSportId) ??
-        null,
-    );
+  const fetchTeams = async (
+    selectedSportId: string,
+    page: number,
+    amount: number,
+  ) => {
     const { data, message, ok } = await TeamService.getTeams(browserClient, {
       sport_id: selectedSportId,
     });
+
     if (data) {
-      setTeams(data);
+      const startIndex = (page - 1) * amount;
+      const endIndex = startIndex + amount;
+      const teamsSubset = data.slice(startIndex, endIndex);
+      setTeams(teamsSubset);
+      setTotalTeam(data.length);
     } else {
       toast.error("Error buscando equipos", message);
       setTeams([]);
     }
+  };
+
+  useEffect(() => {
+    router.push(
+      `/equipos?sport=${selectedSport?.name}&page=${page}&amount=${amount}`,
+      undefined,
+      {
+        shallow: true, // esto es para que solamente recargue la url
+      },
+    );
+    fetchTeams(selectedSport?.id!, page, amount);
+  }, [page, amount, selectedSport]);
+
+  async function handleChangeSportSelect(selectedSportId: string) {
+    setPage(1);
+    setSelectedSport(
+      sportsFromDb.find((sportFromDb) => sportFromDb.id === selectedSportId) ??
+        null,
+    );
+    reset();
+    fetchTeams(selectedSportId, 1, amount);
   }
 
   function handleSearchTeam(event: React.ChangeEvent<HTMLInputElement>) {
@@ -83,6 +121,32 @@ function Teams({ teamsFromDb, defaultSportFromDb, sportsFromDb }: Props) {
     setTeams(filteredTeams);
   }
 
+  const handlePaginationChange = (newPage: number) => {
+    setPage(newPage);
+    router.push(
+      `/equipos?sport=${selectedSport?.name}&page=${newPage}&amount=${amount}`,
+      undefined,
+    );
+    fetchTeams(selectedSport?.id!, newPage, amount); // Fetch teams when pagination changes
+  };
+
+  const handleItemsPerPageChange = (newValue: number) => {
+    setAmount(newValue);
+    setPage(1);
+    router.push(
+      `/equipos?sport=${selectedSport?.name}&page=${1}&amount=${newValue}`,
+      undefined,
+    );
+  };
+
+  const reset = () => {
+    setSearchTeam("");
+    setPage(1);
+  };
+  useEffect(() => {
+    console.log("Selected Sport:", selectedSport);
+    console.log("Teams:", teams);
+  }, [selectedSport, teams]);
   return (
     <Layout breadcrumbs={[{ name: "Equipos", href: "/equipos" }]}>
       <div>
@@ -128,6 +192,22 @@ function Teams({ teamsFromDb, defaultSportFromDb, sportsFromDb }: Props) {
       ) : (
         <p>No se encontraron equipos.</p>
       )}
+      <div className="mt-4 flex justify-center">
+        <Pagination
+          total={Math.ceil(totalTeam / amount)}
+          value={page}
+          onChange={handlePaginationChange}
+        />
+        <Select
+          className="ml-2"
+          data={[1, 20, 30].map((value) => ({
+            value: value.toString(),
+            label: `${value}`,
+          }))}
+          placeholder={`Items por página: ${amount}`}
+          onChange={(value) => handleItemsPerPageChange(Number(value))}
+        />
+      </div>
     </Layout>
   );
 }
