e46158a7f5a5f69a490c9cfe4fe38cddce7103b4
Adriano <adricatena@gmail.com>
2024-01-05 17:06:10 -0300

    add basket fem to create teams

--- src/pages/equipos/crear/index.tsx
+++ src/pages/equipos/crear/index.tsx
@@ -1,53 +1,51 @@
+/* eslint-disable @next/next/no-img-element */
 import Layout from "@/components/layout";
-import ULink from "@/components/unstyled-link";
-import Image from "next/image";
+import Link from "next/link";
+import logoBasketFem from "../../../../public/logo-basket-fem.webp";
+import logoBasket from "../../../../public/logo-basket.webp";
+import logoF11 from "../../../../public/logo-f11.webp";
+import logoF7 from "../../../../public/logo-f7.webp";
+
+const SPORTS_LINKS: { href: string; imgSrc: string; alt: string }[] = [
+  {
+    href: "/equipos/crear/basket",
+    imgSrc: logoBasket.src,
+    alt: "Logo basket",
+  },
+  {
+    href: "/equipos/crear/basket femenino",
+    imgSrc: logoBasketFem.src,
+    alt: "Logo basket femenino",
+  },
+  {
+    href: "/equipos/crear/futbol 7",
+    imgSrc: logoF7.src,
+    alt: "Logo futbol 7",
+  },
+  {
+    href: "/equipos/crear/futbol 11",
+    imgSrc: logoF11.src,
+    alt: "Logo futbol 11",
+  },
+];
 
 function TeamSports() {
   return (
     <Layout breadcrumbs={[{ name: "Equipos", href: "/equipos/crear" }]}>
-      <div className="flex flex-wrap justify-between">
-        <ULink href="/equipos/crear/basket">
-          <div className="mr-8">
-            <Image
-              src="/logo-basket.webp"
-              alt="Basket"
-              width={300}
-              height={300}
-              priority={true}
-              loader={({ src, width, quality }) => {
-                return `${src}?w=${width}&q=${quality || 75}`;
-              }}
+      <div className="flex h-full w-full  items-center justify-between">
+        {SPORTS_LINKS.map((link) => (
+          <Link
+            key={link.href}
+            href={link.href}
+            className="grid place-items-center"
+          >
+            <img
+              alt={link.alt}
+              src={link.imgSrc}
+              className="w-48 rounded-full"
             />
-          </div>
-        </ULink>
-        <ULink href="/equipos/crear/futbol 7">
-          <div className="mr-8">
-            <Image
-              src="/logo-f7.webp"
-              alt="Futbol 7"
-              width={300}
-              height={300}
-              priority={true}
-              loader={({ src, width, quality }) => {
-                return `${src}?w=${width}&q=${quality || 75}`;
-              }}
-            />
-          </div>
-        </ULink>
-        <ULink href="/equipos/crear/futbol 11">
-          <div className="mr-8">
-            <Image
-              src="/logo-f11.webp"
-              alt="Futbol 11"
-              width={300}
-              height={300}
-              priority={true}
-              loader={({ src, width, quality }) => {
-                return `${src}?w=${width}&q=${quality || 75}`;
-              }}
-            />
-          </div>
-        </ULink>
+          </Link>
+        ))}
       </div>
     </Layout>
   );
